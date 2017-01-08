fab17698899a50fbad753515ee61adf9148e28cc
KingLogan <schwarzl20@bishops.com>
2017-01-08 11:54:13 -0800

    adding dis stuff

---
+++ LogsmGogs/UpdateApp/BindingHelper/RelayCommand.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace BindingHelper
+{
+    public class RelayCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+        Action action;
+
+        public RelayCommand(Action action)
+        {
+            this.action = action;
+        }
+
+        bool ICommand.CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        event EventHandler ICommand.CanExecuteChanged
+        {
+            add { return; }
+            remove { return; }
+        }
+
+        void ICommand.Execute(object parameter)
+        {
+            action();
+        }
+    }
+}
---
+++ LogsmGogs/UpdateApp/UpdateApp/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using BindingHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace UpdateApp
+{
+    public class MainWindowViewModel
+    { 
+        public ICommand ButtonCommand
+        {
+            get { return new RelayCommand(ShowMessage); }
+        }
+
+        public void ShowMessage()
+        {
+            MessageBox.Show("Hello");
+        }
+    }
+}
